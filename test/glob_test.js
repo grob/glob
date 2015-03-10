871fb8e8e9e68b2ea7c20b8323593519b41b4cc8
Robert Gaggl <robert@nomatic.org>
2015-03-10 19:03:49 +0100

    refactoring

--- test/glob_test.js
+++ test/glob_test.js
@@ -22,11 +22,10 @@ var getPath = function() {
 };
 
 var doGlob = function() {
-    var pattern = (arguments.length === 1) ? arguments[0] : fs.join.apply(null, arguments);
-    return glob(fs.join(WORK_DIR, pattern));
+    return glob(getPath.apply(null, arguments));
 };
 
-var eqNoOrder = function(arr1, arr2) {
+var equals = function(arr1, arr2) {
     if (!Array.isArray(arr1)) {
         arr1 = [arr1];
     }
@@ -65,46 +64,40 @@ exports.tearDown = function() {
 };
 
 exports.testGlob = function() {
-    eqNoOrder(doGlob("a"), getPath("a"));
-    eqNoOrder(doGlob("a", "D"), getPath("a", "D"));
-    eqNoOrder(doGlob("aab"), getPath("aab"));
-    eqNoOrder(doGlob("zymurgy"), []);
-    eqNoOrder(doGlob("*"), list());
+    equals(doGlob("a"), getPath("a"));
+    equals(doGlob("a", "D"), getPath("a", "D"));
+    equals(doGlob("aab"), getPath("aab"));
+    equals(doGlob("zymurgy"), []);
+    equals(doGlob("*"), list());
 };
 
 exports.testGlobOneDirectory = function() {
-    eqNoOrder(doGlob("a*"), [getPath("a"), getPath("aab"), getPath("aaa")]);
-    eqNoOrder(doGlob("*a"), [getPath("a"), getPath("aaa")]);
-    eqNoOrder(doGlob(".*"), [getPath(".aa"), getPath(".bb")]);
-    eqNoOrder(doGlob("?aa"), [getPath("aaa")]);
-    eqNoOrder(doGlob("aa?"), [getPath("aaa"), getPath("aab")]);
-    eqNoOrder(doGlob("aa[ab]"), [getPath("aaa"), getPath("aab")]);
-    eqNoOrder(doGlob("q*"), []);
+    equals(doGlob("a*"), [getPath("a"), getPath("aab"), getPath("aaa")]);
+    equals(doGlob("*a"), [getPath("a"), getPath("aaa")]);
+    equals(doGlob(".*"), [getPath(".aa"), getPath(".bb")]);
+    equals(doGlob("?aa"), [getPath("aaa")]);
+    equals(doGlob("aa?"), [getPath("aaa"), getPath("aab")]);
+    equals(doGlob("aa[ab]"), [getPath("aaa"), getPath("aab")]);
+    equals(doGlob("q*"), []);
 };
 
 exports.testGlobNestedDirectory = function() {
-    var isCaseInSensitiveFS = fs.exists(getPath("A"));
-    if (isCaseInSensitiveFS) {
-        eqNoOrder(doGlob("a", "bcd", "E*"), [getPath("a", "bcd", "EF"), getPath("a", "bcd", "efg")]);
-
-    } else {
-        eqNoOrder(doGlob("a", "bcd", "E*"), [getPath("a", "bcd", "EF")]);
-    }
-    eqNoOrder(doGlob("a", "bcd", "*g"), [getPath("a", "bcd", "efg")]);
+    equals(doGlob("a", "bcd", "E*"), [getPath("a", "bcd", "EF")]);
+    equals(doGlob("a", "bcd", "*g"), [getPath("a", "bcd", "efg")]);
 };
 
 exports.testGlobDirectoryNames = function() {
-    eqNoOrder(doGlob("*", "D"), [getPath("a", "D")]);
-    eqNoOrder(doGlob("*", "*a"), []);
-    eqNoOrder(doGlob("a", "*", "*", "*a"), [getPath("a", "bcd", "efg", "ha")]);
-    eqNoOrder(doGlob("?a?", "*F"), [getPath("aaa", "zzzF"), getPath("aab", "F")]);
+    equals(doGlob("*", "D"), [getPath("a", "D")]);
+    equals(doGlob("*", "*a"), []);
+    equals(doGlob("a", "*", "*", "*a"), [getPath("a", "bcd", "efg", "ha")]);
+    equals(doGlob("?a?", "*F"), [getPath("aaa", "zzzF"), getPath("aab", "F")]);
 };
 
 exports.testGlobDirectoryWithTrailingSlash = function() {
     // Patterns ending with a slash shouldn't match non-dirs
-    eqNoOrder(glob(getPath("Z*Z") + separator), []);
-    eqNoOrder(glob(getPath("ZZZ") + separator), []);
+    equals(glob(getPath("Z*Z") + separator), []);
+    equals(glob(getPath("ZZZ") + separator), []);
     // When there is a wildcard pattern which ends with separator, glob()
     // doesn't blow up.
-    eqNoOrder(glob(getPath("aa*") + separator), [getPath("aab"), getPath("aaa")]);
+    equals(glob(getPath("aa*") + separator), [getPath("aab"), getPath("aaa")]);
 };
